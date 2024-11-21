4bc2e38bd2bb0b285fdddc71185d792c9c6e9516
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-11-21 22:33:48 +0100

    Update sub_main.c

--- Dooble/sub_main.c
+++ Dooble/sub_main.c
@@ -1,1 +1,4 @@
-zone pour définir les fonctions à part du main
+// Pour lier un fichier à un main (et donc pouvoir utiliser les fonctions déclaré et "partagé" des fonctions, faire comme ci-dessous
+#include "sub_main.h"
+
+// Création des fonction
