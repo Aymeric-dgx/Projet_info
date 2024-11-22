088b155ce6ccdfe8bcd4a62b2a41277c585eea7c
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-11-22 12:16:19 +0100

    Update main.c

--- Dooble/main.c
+++ Dooble/main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    
+
     // Initialisation du SDL
     SDL_Init(SDL_INIT_EVERYTHING);
 
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
     int screen_height = screen.h;
     int screen_width = screen.w;
 
-    //Création de la fenêtre et du renderer (= moteur de rendu)  (nb : on peut créer plusieurs renderer si on souhaite)
+    //Création fenêtre + renderer (= moteur de rendu)
     SDL_Window* window = SDL_CreateWindow("Dooble",
                                             SDL_WINDOWPOS_UNDEFINED,
                                             SDL_WINDOWPOS_UNDEFINED,
@@ -21,25 +21,15 @@ int main(int argc, char* argv[]) {
                                             SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-
-    //Début mise en forme initial des fenêtres
-
-    // Set up fond en bleu
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);   // Fixe la couleur qui va être utlisié par ce renderer (pour modifier refaire un SetRenderDrawColor)
-    SDL_RenderClear(renderer);                                  //  Efface la cible de rendu actuelle avec la couleur de dessin définie
+    // Début création des strucures
 
     // Création d'un rectangle
     SDL_Rect rect;
-    rect.x = 0;                                                 // Position coin haut gauche en x
-    rect.y = 0;                                                 // Position coin haut gauche en y
-    rect.w = screen_width / 2;                                  // Largeur du rectangle
-    rect.h = screen_height / 2;                                 // Hauteur du rectangle
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);   // Définir la couleur du rectangle (rouge)
-    SDL_RenderDrawRect(renderer, &rect);                        // Dessiner le rectangle (contour)
-    SDL_RenderFillRect(renderer, &rect);                        // Remplir le rectangle
-    SDL_RenderPresent(renderer);                                // Mettre à jour l'affichage
-
-
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = screen_width / 2;
+    rect.h = screen_height / 2;
+    
 
 
 
@@ -48,11 +38,11 @@ int main(int argc, char* argv[]) {
     SDL_Event event;
     while(running) {
         // Gestion des évenements
-        while(SDL_PollEvent(&event)) {                      // SDL_PollEvent : renvoie 1 si un évenement en attente, sinon 0, et donc le code après la boucle peut s'éxecuter
-            if (event.type == SDL_QUIT) {                   // Si alt+f4 ou la croix rouge cliqué, running = 0  -> fin affichage
+        while(SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
                 running = 0;
             }
-            if (event.type == SDL_KEYDOWN) {                // est vrai dès que n'importe quelle touche est pressée
+            if (event.type == SDL_KEYDOWN) {
                 if (event.key.keysym.sym == SDLK_ESCAPE) {
                     running = 0;
                 }
@@ -61,9 +51,18 @@ int main(int argc, char* argv[]) {
 
         // Début code pour l'affichage "dynamique"
 
+        // Set up fond en bleu
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderClear(renderer);
+
+        // Set up rectangle en rouge
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
+        SDL_RenderPresent(renderer);
     }
 
-    
+
     // Destruction propre du SDL
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
