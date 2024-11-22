64b42ca25056938a9f4ae0d87c5a14daef6b2499
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-11-22 16:47:08 +0100

    Update switch+close_x_y_mouse.c

--- Dooble/Exemple/switch+close_x_y_mouse.c
+++ Dooble/Exemple/switch+close_x_y_mouse.c
@@ -92,6 +92,8 @@ int main(int argc, char* argv[]) {
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderDrawRect(renderer, &rect);
         SDL_RenderFillRect(renderer, &rect);
+
+        // Mise à jour fenetre
         SDL_RenderPresent(renderer);
     }
 
