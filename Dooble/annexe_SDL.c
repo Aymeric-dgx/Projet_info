b5ce5990aad7a3534632fb8d8ecc6e39297bb5b1
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-11-24 22:37:02 +0100

    Update annexe_SDL.c

--- Dooble/annexe_SDL.c
+++ Dooble/annexe_SDL.c
@@ -53,7 +53,7 @@ SDL_RenderClear(renderer1);       // Colorie tout les pixels de la fenetre dont 
 SDL_RenderPresent(renderer1);     // Mets a jour la fenetre du renderer afin de pouvoir afficher les modifications faites sur cette derniere
 
 SDL_Rect* rect;     // Strucure permettant de stocker des infos pour un rectangle, .x .y .w et .h
-SDL_RenderDrawRect(renderer, &rect);   // Déssine coutour du rectangle (largeur 1 pixel) avec la couleur du renderer. (on peut pas modifier la largeur -> pas très utile ?)
+SDL_RenderDrawRect(renderer, &rect);  // Déssine coutour du rectangle (largeur 1 pixel) avec la couleur du renderer (useless sauf si rectangle collés pour les différencier)
 SDL_RenderFillRect(renderer, &rect);   // Rempli le rectangle avec la couleur du renderer
 // Pour faire des contours plus épais, écrire une foncton qui va dessiner plusieurs petits rectangele sur les contours d'un plus grand
 SDL_Rect buttons[3];  // Créer un tableau buttons de 3 structure SDL_Rect (= créer un tableau contenant 3 rectangles)
@@ -67,10 +67,19 @@ TTF_RenderText_Solid(bigFont, "Bienvenue", textColor);    // Return du texte en 
 SDL_Texture* titleTexture  // Used to represent image, but + fast que SDL_Surface (directement utilisé par GPU), utilisé pour : rendu sur écran, performances, manip d'images
 SDL_CreateTextureFromSurface(renderer, titleSurface);  // Convertit surface en texture pour rendu + rapide (GPU) : renderer avec qui travailler, surface qu'on veut convertir
 SDL_FreeSurface(titleSurface);  // Free la mémoire alloué à une surface (à libérer par exemple après avoir créer une texture via cette surface, et dont on a donc + besoin) 
+SDL_RenderCopy(renderer, titleTexture, NULL, &titleRect); // Copie une texture dans le rendu actuel (a peu près équivalent à SDL_RenderPresent mais pour des texture)
+// parametres : renderer avec qui travailler, texture qu'on veut copier, si on veut récup. qu'une partie de la texture -> donner pointeur vers un rect de la taiile 
+// qu'on souhaite, pointeur vers un rect où l'on va copier la texture
+SDL_DestroyTexture(titleTexture);  // Detruire proprement la texture
+TTF_CloseFont(bigFont);   // Feremr et libérer la mémoire associée à une police (TTF_Font) précédemment ouverte avec TTF_OpenFont
+TTF_Quit();    // Fermer et nettoyer la lib SDL_ttf
 
 SDL_Event event;      // Structure permettant de stocker des informations sur les événements dans SDL (entrées clavier, mouvements de souris,événements de fenêtre, ...)
 SDL_PollEvent(&event) // Vérifie si evenement en attente. Si oui, il est retiré de file d'attente et stocké dans event. Retourne 1 si un événement a été récupéré, ou 0 sinon
 event.key.keysym.sym  // permet d'identifier la touche spécifique qui a été pressée ou relâchée lors d'un événement clavier
+int mouseX = event.button.x, mouseY = event.button.y; // (à verifier), si event == MOUSEBUTTONDOWN (ou UP), permet de récupérer les coordonnées de la souris
+
+SDL_Log("Bouton 'Jouer' cliqué !") // Ecrit un message de journalisation dans la console
 
 SDL_DestroyRenderer(renderer1);  // Permet de détruire proprement un renderer (faire de préférence avant de détruire la fenetre associé)
 SDL_DestroyWindow(window1);     // Permet de détruire proprement une fenetre (faire de préférence apres avoir détruit le renderer associé)
