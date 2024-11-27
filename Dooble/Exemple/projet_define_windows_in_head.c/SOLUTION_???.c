2b55c776508c371f0afa7a2aa0a964472d889355
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-11-27 13:55:02 +0100

    Create SOLUTION_???.c

---
+++ Dooble/Exemple/projet_define_windows_in_head.c/SOLUTION_???.c
@@ -0,0 +1,4 @@
+//Plutot que de s emmerder a retourenr un pointeur de pointeur de strucuture et tout le bordel, il y aurait beacoup plus simple, on ferait plutot :
+
+int running = 0;
+while (running) 
