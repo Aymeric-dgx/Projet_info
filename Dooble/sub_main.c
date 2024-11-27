d57f83fe4fa0ce2d220aae316807f2cbde166e54
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-11-27 10:50:08 +0100

    Delete Dooble/sub_main.c

--- Dooble/sub_main.c
+++
@@ -1,4 +0,0 @@
-// Pour lier un fichier à un main (et donc pouvoir utiliser les fonctions déclaré et "partagé" des fonctions, faire comme ci-dessous
-#include "sub_main.h"
-
-// Création des fonction
