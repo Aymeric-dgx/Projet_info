06a669c6c1906812d6f02a47fe884e0571e5ae85
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-03 17:55:00 +0100

    Update 1-main.c

--- Dooble/1-main.c
+++ Dooble/1-main.c
@@ -3,6 +3,13 @@
 #include <SDL_ttf.h>
 #include "option.h"
 
+/* Aymeric : renommer les fichiers
+main
+1-menu
+2-start
+pour qu il corresponde au "numero" de leur fenetre respective, pour que ce soit moins ambigue ?
+*/
+
 
 int main(int argc, char* argv[]){
     SDL_Init(SDL_INIT_EVERYTHING);
