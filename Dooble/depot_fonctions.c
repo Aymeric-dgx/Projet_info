3e8df7dc17ac710b3e94a48eb297ebf9a9aa0505
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-04 14:12:05 +0100

    Update depot_fonctions.c

--- Dooble/depot_fonctions.c
+++ Dooble/depot_fonctions.c
@@ -1,1 +1,16 @@
-// Espace pour y deposer des petites fonctions qu on a fait
+// Espace pour y deposer des petites fonctions qu on a fait (ne pas oublier de les déclarer dans option.h)
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include "option.h"
+
+
+// Fonction qui verifie si un click est dans un rectangle, et renvoie 1 ou 0
+// Parametre : posistions x et y de la souris, le rectangle dans lequelle on veut verifier le click
+int click_in_rect(int mouse_x, int mouse_y, SDL_Rect rect) {
+    if (mouse_x >= rect.x && mouse_x <= rect.x+rect.w &&
+        mouse_y >= rect.y && mouse_y <= rect.y+rect.h) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
