796fe6c3954ad3d54969c939dbf38b219c273204
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-06 10:54:41 +0100

    Update option.h

--- Dooble/option.h
+++ Dooble/option.h
@@ -7,4 +7,8 @@ int window_play();
 // Petite fonctions utilitaires
 int click_in_rect(int mouse_x, int mouse_y, SDL_Rect rect)
 
+void edit_progress_bar_with_click(SDL_Rect bar, SDL_Rect* sub_bar, int mouse_x, int mouse_y, int* ratio_bar)
+void edit_progress_bar_with_ratio(SDL_Rect bar, SDL_Rect* sub_bar, int ratio)
+void maj_progress_bar(SDL_Renderer* renderer, SDL_Rect bar, SDL_Rect sub_bar, SDL_Color bar_color, SDL_Color sub_bar_color)
+
 #endif();
