31b218d47403bfaf37049c7fd5d47a2630f80c1f
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-06 14:01:22 +0100

    Update option.h

--- Dooble/option.h
+++ Dooble/option.h
@@ -9,6 +9,8 @@ int click_in_rect(int mouse_x, int mouse_y, SDL_Rect rect)
 
 void edit_progress_bar_with_click(SDL_Rect bar, SDL_Rect* sub_bar, int mouse_x, int mouse_y, int* ratio_bar)
 void edit_progress_bar_with_ratio(SDL_Rect bar, SDL_Rect* sub_bar, int ratio)
-void maj_progress_bar(SDL_Renderer* renderer, SDL_Rect bar, SDL_Rect sub_bar, SDL_Color bar_color, SDL_Color sub_bar_color)
+void maj_progress_bar(SDL_Renderer* renderer, SDL_Rect bar, SDL_Rect sub_bar, SDL_Color bar_color, SDL_Color sub_bar_color)77
+
+void activate_input_box(SDL_Event* event, int* is_active, char* input_text, SDL_Rect input_box)
 
 #endif();
