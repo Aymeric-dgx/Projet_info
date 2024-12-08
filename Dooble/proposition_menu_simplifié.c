64fa18e6ef91a91e0963b3f8b76c012af272ac53
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-08 20:38:21 +0100

    Update proposition_menu_simplifié.c

--- Dooble/proposition_menu_simplifié.c
+++ Dooble/proposition_menu_simplifié.c
@@ -17,50 +17,39 @@ int main() {
     SDL_GetCurrentDisplayMode(0, &screen);
 
 
-    // Création fenetre + renderer + chargement police
+    // Création fenetre + renderer + chargement police (qui s'adaptera à l'écran)
     SDL_Window* window_menu = SDL_CreateWindow("Menu",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
                                           screen.w, screen.h,
                                           SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer_menu = SDL_CreateRenderer(window_menu, -1, SDL_RENDERER_ACCELERATED);
-    TTF_Font* bigFont = TTF_OpenFont("../police/arial.ttf", (screen.w / 250) * 24); // Police grande taille (taille qui s'adapte à la taille de l'écan)
-    TTF_Font* smallFont = TTF_OpenFont("../police/arial.ttf", (screen.w / 450) * 24); // Police petite taille (taille qui s'adapte à la taille de l'écan)
+    TTF_Font* bigFont = TTF_OpenFont("../police/arial.ttf", (screen.w / 250) * 24); // Police grande taille
+    TTF_Font* smallFont = TTF_OpenFont("../police/arial.ttf", (screen.w / 450) * 24); // Police petite taille
 
+    // Création des boutons
 
-    // Texte "Dooble" : surface + texture + position (rectangle)
-    SDL_Color tittleTextColor = {255, 255, 255, 255};   // Blanc
-    SDL_Surface* tittleSurface = TTF_RenderText_Solid(bigFont, "Dooble", tittleTextColor);
-    SDL_Texture* tittleTexture = SDL_CreateTextureFromSurface(renderer_menu, tittleSurface);
-    SDL_Rect titleRect = {screen.w/2 - tittleSurface->w/2, screen.h/8 - tittleSurface->h/2.5, tittleSurface->w, tittleSurface->h};
-    SDL_FreeSurface(tittleSurface);
-
-
-    // Création des boutons Play, Score, Quit
-    SDL_Color buttonTextColor = {0, 0, 0, 255}; // Noir
-    char* buttonNames[3] = {"Play", "Score", "Quit"};   // Pointeur de chaine de caractère
-    SDL_Rect buttonRects[3];    // Tableau des "gros rectangles" pour les boutons, qui feront le fond des boutons
-    SDL_Rect textButtonRects[3];    // Tableau des rectangles pour le placement du texte (pour les placer dans les gros rectangles)
-    SDL_Texture* buttonTextures[3]; // Tableau pour stocker les textures pour les boutons
+    SDL_Color button_text_color = {0,0,0,255};
+    SDL_Color button_color = {255,255,255,255};
+    SDL_Rect button_rects[3];
+    SDL_Rect text_button_rects[3];
+    char* button_names[3] = {"Play", "Scores", "Quit"};
 
     for(int i=0 ; i<3 ; i++) {
-        // Création du texte des boutons
-        SDL_Surface* buttonSurface = TTF_RenderText_Solid(smallFont, buttonNames[i], buttonTextColor);  // police, texte, couleur
-        buttonTextures[i] = SDL_CreateTextureFromSurface(renderer_menu, buttonSurface); // renderer, surface
 
         // Placement des boutons (= "gros rectangles")
-        buttonRects[i].w = screen.w / 3;
-        buttonRects[i].h = screen.h/ 6.5;
-        buttonRects[i].x = screen.w/2 - buttonRects[i].w/2;
-        buttonRects[i].y = buttonRects[i].h*3 - screen.h/8 + i*screen.h/5.5;    // Espacement vertical entre chaque boutons
+        button_rects[i].w = screen.w / 3;
+        button_rects[i].h = screen.h/ 6.5;
+        button_rects[i].x = screen.w/2 - button_rects[i].w/2;
+        button_rects[i].y = button_rects[i].h*3 - screen.h/8 + i*screen.h/5.5;    // Espacement vertical entre chaque boutons
 
         // Placement des "petits rectangle" dans les boutons
-        textButtonRects[i].w = buttonSurface->w;
-        textButtonRects[i].h = buttonSurface->h;
-        textButtonRects[i].x = buttonRects[i].x + (buttonRects[i].w - textButtonRects[i].w)/2;
-        textButtonRects[i].y = buttonRects[i].y + (buttonRects[i].h - textButtonRects[i].h)/2;
-
-        SDL_FreeSurface(buttonSurface);
+        SDL_Surface* tmp_surface = TTF_RenderText_Solid(smallFont, button_names[i], button_text_color);
+        text_button_rects[i].w = tmp_surface->w;
+        text_button_rects[i].h = tmp_surface->h;
+        text_button_rects[i].x = button_rects[i].x + (button_rects[i].w - text_button_rects[i].w)/2;
+        text_button_rects[i].y = button_rects[i].y + (button_rects[i].h - text_button_rects[i].h)/2;
+        SDL_FreeSurface(tmp_surface);
     }
 
 
@@ -78,15 +67,15 @@ int main() {
             else if(event.type == SDL_MOUSEBUTTONDOWN) {
                 int mouse_x, mouse_y;
                 SDL_GetMouseState(&mouse_x, &mouse_y);
-                if (click_in_rect(mouse_x, mouse_y, buttonRects[0])) {  // Bouton "Play" cliqué
+                if (click_in_rect(mouse_x, mouse_y, button_rects[0])) {  // Bouton "Play" cliqué
                     printf("Bouton Play clique\n");
                     returning = 1;
                 }
-                else if (click_in_rect(mouse_x, mouse_y, buttonRects[1])) {  // Bouton "Scores" cliqué
+                else if (click_in_rect(mouse_x, mouse_y, button_rects[1])) {  // Bouton "Scores" cliqué
                     printf("Bouton Scores clique\n");
                     returning = 2;
                 }
-                else if (click_in_rect(mouse_x, mouse_y, buttonRects[2])) {  // Bouton "Quitter" cliqué
+                else if (click_in_rect(mouse_x, mouse_y, button_rects[2])) {  // Bouton "Quitter" cliqué
                     printf("Bouton Quitter clique\n");
                     returning = 0;
                     running = 0;
@@ -96,18 +85,22 @@ int main() {
         // Fin gestion des évements
 
         //Peinture du fond
-        SDL_SetRenderDrawColor(renderer_menu,0,0,50,255);
+        SDL_Color renderer_color = {0,0,50,255};
+        SDL_SetRenderDrawColor(renderer_menu,renderer_color.r, renderer_color.g, renderer_color.b, renderer_color.a);
         SDL_RenderClear(renderer_menu);
 
-        // Dessiner le texte principal "Dooble"
-        SDL_RenderCopy(renderer_menu, tittleTexture, NULL, &titleRect);
+        // Création text "Dooble"
+        SDL_Color title_text_color = {255, 255, 255, 255};
+        SDL_Surface* tmp_surface = TTF_RenderText_Solid(bigFont, "Dooble", title_text_color);   // Surface "temporaire" pour placer correctement le texte au centre
+        SDL_Rect title_rect = {screen.w/2 - tmp_surface->w/2, screen.h/8 - tmp_surface->h/2.5, tmp_surface->w, tmp_surface->h};
+        SDL_Rect text_title_rect;
+        SDL_FreeSurface(tmp_surface);
+        create_button(renderer_menu, title_rect, text_title_rect, "Dooble", bigFont, title_text_color, renderer_color);
 
-        // Dessiner les boutons
-        for (int i=0 ; i<3 ; i++) {
-            SDL_SetRenderDrawColor(renderer_menu,255,255,255,255);  // Blanc
-            SDL_RenderFillRect(renderer_menu, &buttonRects[i]);             // Remplir les "gros" carré --> le fond des boutons
-            SDL_SetRenderDrawColor(renderer_menu,0,0,0,255);       // Noir
-            SDL_RenderCopy(renderer_menu, buttonTextures[i], NULL, &textButtonRects[i]);    // "Imprimer"" le texte
+
+        // Création des boutons
+        for(int i=0 ; i<3 ; i++) {
+            create_button(renderer_menu, button_rects[i], text_button_rects[i], button_names[i], smallFont, button_text_color, button_color);
         }
 
         // Mis à jour de l'écran
@@ -117,7 +110,7 @@ int main() {
 
     if (returning != -1) {
         return returning;
-    } else {
+        } else {
         printf("pb avec le returning");
         return -1;
     }
