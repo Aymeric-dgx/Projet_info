1fe1aa06acd366b1b1e86875113e95d7e42fe7f2
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-08 18:44:14 +0100

    Create Create_button.c

---
+++ Dooble/Exemple/Applications_fonctions/Create_button.c
@@ -0,0 +1,42 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "option.h"
+
+
+int main() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+
+    SDL_DisplayMode screen;
+    SDL_GetCurrentDisplayMode(0, &screen);
+
+    SDL_Window* window = SDL_CreateWindow("Dooble",
+                                             SDL_WINDOWPOS_CENTERED,
+                                             SDL_WINDOWPOS_CENTERED,
+                                             screen.w, screen.h,
+                                             SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    TTF_Font* font = TTF_OpenFont("../police/arial.ttf", 64);
+
+
+    int running = 1;
+    SDL_Event event;
+
+    while(running) {
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+        SDL_Rect button_rect = {100,100,800,500};
+        SDL_Rect text_rect;
+        SDL_Color font_color = {255,255,0,255};
+        SDL_Color button_rect_color = {0,255,255,255};
+        create_button(renderer, button_rect, text_rect, "Hello test", font, font_color, button_rect_color);
+        SDL_RenderPresent(renderer);
+    }
+}
