3a2e5d74e3cba42ea303f86d80bcaf85af79ffbb
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-20 15:58:38 +0100

    Update 0 - main.c

--- Dooble/0 - main.c
+++ Dooble/0 - main.c
@@ -1,19 +1,6 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <SDL_ttf.h>
-#include "option.h"
-
-/* nb_window :
-0 : Quitter (stopper le programme)
-1 : menu
-2 : options
-3 : game    (ou 3 : solo, 4 : multi ?)
-4 : scores
-*/
-
-#define SDL_MAIN_HANDLED
-#include <SDL.h>
-#include <SDL_ttf.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -27,34 +14,27 @@ void main(){
     SDL_DisplayMode screen;
     SDL_GetCurrentDisplayMode(0, &screen);
 
-    int general_nb_window = 1;
+
+    int nb_window = 1;
+    int nb_player;
+    int time;
+    int gamemode;
+    int nb_words;
+    char** names_player_save = malloc(sizeof(char*)*15);
+    for(int i=0 ; i<15 ; i++) {
+        char* tmp = malloc(sizeof(char)*50);
+        tmp[0] = '\0';
+        names_player_save[i] = tmp;
+    }
 
     int running = 1;
 
     while (running) {
         // Gestion des événements
-        if (general_nb_window == 0) running = 0; // Bouton Quitter cliqué --> stopper le programme
-        if(general_nb_window == 1) {
-            int nb_window = general_nb_window;
-            window_menu(screen, &nb_window);
-            general_nb_window = nb_window;
-        }
-        if (general_nb_window == 2) {
-            int nb_window = general_nb_window;
-            int nb_player;
-            int time;
-            int gamemode;
-            int nb_words;
-            char** names_player_save = malloc(sizeof(char*)*15);
-            for(int i=0 ; i<15 ; i++) {
-                char* tmp = malloc(sizeof(char)*50);
-                tmp[0] = '\0';
-                names_player_save[i] = tmp;
-            }
-            options_window(screen, &nb_window, &nb_player, &time, &nb_words, &gamemode, names_player_save);
-            general_nb_window = nb_window;
-        }
-        if(general_nb_window == 3) running = 0;
+        if (nb_window == 0) running = 0; // Bouton Quitter cliqué --> stopper le programme
+        if(nb_window == 1) window_menu(screen, &nb_window);
+        if (nb_window == 2) options_window(screen, &nb_window, &nb_player, &time, &nb_words, &gamemode, names_player_save);
+        if(nb_window == 3) running = 0;
     }
 
 
