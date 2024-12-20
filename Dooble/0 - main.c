fe63ad33bf398353c97486bb594d5d3dbce44f1f
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2024-12-20 15:43:35 +0100

    Update 0 - main.c

--- Dooble/0 - main.c
+++ Dooble/0 - main.c
@@ -11,40 +11,53 @@
 4 : scores
 */
 
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "option.h"
 
-int main(int argc, char* argv[]){
+
+void main(){
     SDL_Init(SDL_INIT_EVERYTHING);
     TTF_Init();
     // Obtenir les dimensions de l'écran
     SDL_DisplayMode screen;
     SDL_GetCurrentDisplayMode(0, &screen);
-    int screen_width = screen.w;
-    int screen_height = screen.h;
 
+    int general_nb_window = 1;
 
     int running = 1;
-    SDL_Event event;    // On en pas besoin pour l'instant
-
-
-    int nb_window = window_menu(screen_width, screen_height);
 
     while (running) {
         // Gestion des événements
-        if (nb_window == 0) {    // Bouton Quitter cliqué --> stopper le programme
-            running = 0;
+        if (general_nb_window == 0) running = 0; // Bouton Quitter cliqué --> stopper le programme
+        if(general_nb_window == 1) {
+            int nb_window = general_nb_window;
+            window_menu(screen, &nb_window);
+            general_nb_window = nb_window;
         }
-        else if (nb_window == 1) {    // La fenetre menu est ouverte
-            nb_window = window_play(screen_width, screen_height);
-            running = 0;
+        if (general_nb_window == 2) {
+            int nb_window = general_nb_window;
+            int nb_player;
+            int time;
+            int gamemode;
+            int nb_words;
+            char** names_player_save = malloc(sizeof(char*)*15);
+            for(int i=0 ; i<15 ; i++) {
+                char* tmp = malloc(sizeof(char)*50);
+                tmp[0] = '\0';
+                names_player_save[i] = tmp;
+            }
+            options_window(screen, &nb_window, &nb_player, &time, &nb_words, &gamemode, names_player_save);
+            general_nb_window = nb_window;
         }
-        else if (nb_window == 2) {
-            running = 0;
-        }
+        if(general_nb_window == 3) running = 0;
     }
 
 
     TTF_Quit();
     SDL_Quit();
-
-    return 0;
 }
